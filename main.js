7903f8e207375b090678f30641b33f7271207c43
Jazmin <you@example.com>
2022-02-07 19:01:02 -0300

    arreglos js

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ const dateYear = document.getElementById('dateYear');
 // Tasks Container
 const tasksContainer = document.getElementById('tasksContainer');
 
+
 const setDate = () => {
     const date = new Date();
     dateNumber.textContent = date.toLocaleString('es', { day: 'numeric' });
@@ -14,6 +15,24 @@ const setDate = () => {
     dateMonth.textContent = date.toLocaleString('es', { month: 'short' });
     dateYear.textContent = date.toLocaleString('es', { year: 'numeric' });
 };
+
+setDate();
+
+const addNewTask = document.getElementById('complete-form');
+
+addNewTask.addEventListener('click', (e) => {
+    e.preventDefault();
+    const text = input.value;
+    const li = document.createElement('li');
+    const p = document.createElement('p');
+    p.textContent = text;
+
+});
+
+
+
+
+/*
 
 const addNewTask = event => {
     event.preventDefault();
@@ -44,4 +63,24 @@ const renderOrderedTasks = () => {
     order().forEach(el => tasksContainer.appendChild(el));
 };
 
-setDate();
+
+
+
+/*
+Lo que quiero: Crear una nueva tarea que este clasificada por colores
+segun importancia
+
+    rojo: urgente
+    amarillo: no muy urgente
+    verde: puede esperar
+
+Cuando se hace una tarea nueva, debe quedar al principio
+
+Las tareas se deben almacenar en una caja abajo donde:
+- Las tareas se puedan editar
+- Las tareas se puedan borrar
+- Las tareas se puedan marcar como completas
+    Las tareas completadas se deben mandar abajo de todo
+- Las tareas se les pueda cambiar el color 
+
+*/
