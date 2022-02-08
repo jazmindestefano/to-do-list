f982e2c92bedb0e4b7b76ef36bb9a0e5a22aeb31
Jazmin <you@example.com>
2022-02-08 14:30:38 -0300

    addEventListener to load page and submit button

---
+++ main.js
@@ -0,0 +1,12 @@
+'use strict'
+window.addEventListener('load', () => {
+    const form = document.querySelector('#new-task-form');
+    const input = document.querySelector('#new-task-input');
+    const list_element = document.querySelector('#tasks');
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        console.log("submit form");
+    });
+});
